019bb19ad533e392c90ab6a35031f0ed7c615cbf
Geoff Sobering <geoff@geoff-s.net>
2011-06-21 18:24:59 -0500

    Added code to apply a "hash" to the url and use that to load a particular location, so the URL is bookmarkable. Added code to use a built-in locations-list if the on-line XML file can't be loaded (for example, if the site is being tested locally on a PC).

--- Locations/index.js
+++ Locations/index.js
@@ -1,12 +1,20 @@
 function initialize() {
+   var baseURL = getBaseURL();
+   var basePath = window.location.pathname;
+   var initLocId=getLocationFromURL();
+   
+   var htmlTitle = document.title;
+   
    var resetCenter = new google.maps.LatLng(43.029, -89.37);
    var resetZoom = 10;
    var resetAction = function (event) {
          map.setCenter(resetCenter);
          map.setZoom(resetZoom);
+         document.title = htmlTitle;
+         window.location.hash = null;
          return false;
-   }
-
+   };
+   
    if (window.XMLHttpRequest)
    {// code for IE7+, Firefox, Chrome, Opera, Safari
       xmlhttp=new XMLHttpRequest();
@@ -15,10 +23,28 @@ function initialize() {
    {// code for IE6, IE5
       xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
    }
-   xmlhttp.open("GET", "http://badgerblokartclub.org/Locations/Locations.xml", false);
-   xmlhttp.send();
-   locationsXmlDoc = xmlhttp.responseXML;
-
+   
+   try {
+      xmlhttp.open("GET", "http://badgerblokartclub.org/Locations/Locations.xml", false);
+      xmlhttp.send();
+      locationsXmlDoc = xmlhttp.responseXML;
+   }
+   catch(e) {
+      alert("Can't Load File From Host - using built-in locations list.");
+      var txt='<locations>';
+      txt=txt+'   <location id="matc" name="MATC Lot" lat="43.12274" lng="-89.33206" mapCtrLat="43.12178" mapCtrLng="-89.3325" zoom="17" />';
+      txt=txt+'   <location id="dmv" name="DMV Lot" lat="43.07407" lng="-89.45797" zoom="18" />';
+      txt=txt+'   <location id="cub" name="Cub Lot" lat="43.03410" lng="-89.45851" zoom="18" />';
+      txt=txt+'   <location id="uw" name="UW Health Lot" lat="43.10335" lng="-89.52278" zoom="18" />';
+      txt=txt+'   <location id="costco" name="Costco Lot" lat="43.10094" lng="-89.52262" zoom="18" />';
+      txt=txt+'   <location id="edgerton" name="Edgerton Lot" lat="42.83733" lng="-89.08878" zoom="17" />';
+      txt=txt+'   <location id="wps" name="WPS Lot" lat="43.04494" lng="-89.34380" zoom="18" />';
+      txt=txt+'</locations>';
+      
+      parser=new DOMParser();
+      locationsXmlDoc=parser.parseFromString(txt,"text/xml");
+   }
+   
    locations = locationsXmlDoc.getElementsByTagName("location");
 
    var scaleOptions = {
@@ -37,17 +63,29 @@ function initialize() {
 
    var resetLink = document.getElementById("resetLink");
    resetLink.onclick = resetAction;
+   
+   // Null-Object pattern.
+   // Action to perform if there is no initLoc from the URL.
+   var initLoc = {
+      action: function doNothing(event){ return false;}
+   };
 
-   //for each(var item in locations) processLocationXml(item);
    var len=locations.length;
    for(var i=0; i<len; i++) {
    	var item = locations[i];
    	processLocationXml(item);
    }
    
+   initLoc.action();
+   
+   //VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+   // Just function definitions below...
+   
+   // Side-effect warning: sets the global variable; initLoc
    function processLocationXml(item) {
       var attributes = item.attributes;
       
+      var id = attributes.getNamedItem("id").value;
       var name = attributes.getNamedItem("name").value;
       var lat  = parseFloat(attributes.getNamedItem("lat").value);
       var lng  = parseFloat(attributes.getNamedItem("lng").value);
@@ -64,17 +102,23 @@ function initialize() {
          mapCtrLng = lng;
       }
       var zoom = parseInt(attributes.getNamedItem("zoom").value);
-      var loc = new Location(name, lat, lng, mapCtrLat, mapCtrLng, zoom);
+      var loc = new Location(id, name, lat, lng, mapCtrLat, mapCtrLng, zoom);
       
       add_li(loc);
       makeMarker(loc, map);
+      
+      if(initLocId == id) {
+         initLoc = loc;
+      }
    };
 
-   function Location(name, lat, lng, mapCtrLat, mapCtrLng, zoom) {
+   function Location(id, name, lat, lng, mapCtrLat, mapCtrLng, zoom) {
+      this.id = id;
       this.name = name;
       this.latlng = new google.maps.LatLng(lat, lng);
       this.mapCtr = new google.maps.LatLng(mapCtrLat, mapCtrLng);
       this.zoom = zoom;
+      this.path = basePath+"#"+this.id;
       this.action = makeLocationAction(this);
    }
 
@@ -95,6 +139,8 @@ function initialize() {
       return function doSomething(event) {
          map.setCenter(mapCtr);
          map.setZoom(zoom);
+         document.title = htmlTitle+": "+location.name;
+         window.location.hash = location.id;
          return false;
       }
    }
@@ -103,12 +149,22 @@ function initialize() {
       var list = document.getElementById("locationList");
 
       var a = document.createElement('a');
-      a.setAttribute('href', 'noscript.html');
+      a.setAttribute('href', location.path);
       a.onclick = location.action;
       a.innerHTML = location.name;
 
       var li = document.createElement("li");
       li.insertBefore(a, null);
       list.appendChild(li);
+   }
+   
+   function getLocationFromURL(){
+      return window.location.hash.substring(1);
+   }
+   
+   function getBaseURL() {
+         var query = window.location.href;
+         var vars = query.split("#");
+         return vars[0];
    }
 }
