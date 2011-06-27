708e1163dbe871c1ec10f62ee42d67853e7563b8
Geoff Sobering <geoff@geoff-s.net>
2011-06-27 15:48:55 -0500

    Modified to get the "region" from the XML file.

--- Locations/index.js
+++ Locations/index.js
@@ -3,22 +3,6 @@ function initialize() {
    var basePath = window.location.pathname;
    var initLocId=getLocationFromURL();
    
-   var htmlTitle = document.title;
-   var areaTitle = "Region View";
-   
-   var resetCenter = new google.maps.LatLng(43.029, -89.37);
-   var resetZoom = 10;
-   
-   var resetAction = function (event) {
-         map.setCenter(resetCenter);
-         map.setZoom(resetZoom);
-         document.title = htmlTitle;
-         window.location.hash = null;
-         setMapTitle(areaTitle);
-         resetLink.href = basePath+"#";
-         return false;
-   };
-   
    if (window.XMLHttpRequest)
    {// code for IE7+, Firefox, Chrome, Opera, Safari
       xmlhttp=new XMLHttpRequest();
@@ -34,60 +18,73 @@ function initialize() {
       locationsXmlDoc = xmlhttp.responseXML;
    }
    catch(e) {
-      alert("Can't Load File From Host - using built-in locations list.");
-      var txt='<locations>';
-      txt=txt+'   <location id="matc" name="MATC Lot" latLng="43.12274, -89.33206" mapCtrLatLng="43.12178, -89.3325" zoom="17" />';
-      txt=txt+'   <location id="dmv" name="DMV Lot" latLng="43.07407, -89.45797" zoom="18" />';
-      txt=txt+'   <location id="cub" name="Cub Lot" latLng="43.03410, -89.45851" zoom="18" />';
-      txt=txt+'   <location id="uw" name="UW Health Lot" latLng="43.10335, -89.52278" mapCtrLatLng="43.1027, -89.52245" zoom="18" />';
-      txt=txt+'   <location id="costco" name="Costco Lot" latLng="43.10094, -89.52262" zoom="18" />';
-      txt=txt+'   <location id="edgerton" name="Edgerton Lot" latLng="42.83733, -89.08878" zoom="17" />';
-      txt=txt+'   <location id="wps" name="WPS Lot" latLng="43.04494, -89.34380" zoom="18" />';
-      txt=txt+'</locations>';
+      alert("WARNING: Can't Load File From Host - using built-in locations list.");
+      var txt='<regions>';
+      txt=txt+'   <region id="bbc" name="Badger blokart® Club" latLng="43.029, -89.37" zoom="10" >';
+      txt=txt+'      <locations>';
+      txt=txt+'         <location id="matc" name="MATC Lot" latLng="43.12274, -89.33206" mapCtrLatLng="43.12178, -89.3325" zoom="17" />';
+      txt=txt+'         <location id="dmv" name="DMV Lot" latLng="43.07407, -89.45797" zoom="18" />';
+      txt=txt+'         <location id="cub" name="Cub Lot" latLng="43.03410, -89.45851" zoom="18" />';
+      txt=txt+'         <location id="uw" name="UW Health Lot" latLng="43.10335, -89.52278" mapCtrLatLng="43.1027, -89.52245" zoom="18" />';
+      txt=txt+'         <location id="costco" name="Costco Lot" latLng="43.10094, -89.52262" zoom="18" />';
+      txt=txt+'         <location id="edgerton" name="Edgerton Lot" latLng="42.83733, -89.08878" zoom="17" />';
+      txt=txt+'         <location id="wps" name="WPS Lot" latLng="43.04494, -89.34380" zoom="18" />';
+      txt=txt+'      </locations>';
+      txt=txt+'   </region>';
+      txt=txt+'</regions>';
       
       parser=new DOMParser();
       locationsXmlDoc=parser.parseFromString(txt,"text/xml");
    }
    
-   locations = locationsXmlDoc.getElementsByTagName("location");
+   regions = locationsXmlDoc.getElementsByTagName("region");
+   regionElem = regions[0];
+   locationElems = regionElem.getElementsByTagName("location");
 
-   var scaleOptions = {
-      position: google.maps.ControlPosition.TOP_CENTER
-   };
-
-   var myOptions = {
-      zoom: resetZoom,
-      center: resetCenter,
-      mapTypeId: google.maps.MapTypeId.SATELLITE,
-      scaleControl: true,
-      scaleControlOptions: scaleOptions
-   };
-
-   var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+   var map = makeMap();
+   
+   var region = makeLocationFromXml(null, regionElem);
 
    var resetLink = document.getElementById("resetLink");
-   resetLink.onclick = resetAction;
-   resetLink.href = basePath+"#";
+   modifyLocationLink(resetLink, region);
+   resetLink.innerHTML = resetLink.innerHTML+" Area:";
    
    // Null-Object pattern.
    // Action to perform if there is no initLoc from the URL.
    var initLoc = {
-      action: resetAction
+      id: "Initial",
+      action: region.action
    };
 
-   var len=locations.length;
+   var len=locationElems.length;
    for(var i=0; i<len; i++) {
-   	var item = locations[i];
-   	processLocationXml(item);
+   	var item = locationElems[i];
+   	var loc = makeLocationFromXml(region, item)
+   	processLocation(loc, map);
+   	updateInitLoc(loc);
    }
-   
    initLoc.action();
    
    //VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
    // Just function definitions below...
    
-   // Side-effect warning: sets the global variable; initLoc
-   function processLocationXml(item) {
+   function makeMap() {
+      var scaleOptions = {
+         position: google.maps.ControlPosition.TOP_CENTER
+      };
+      
+      var myOptions = {
+         zoom: 4,
+         center: new google.maps.LatLng(40.0, -99.0),
+         mapTypeId: google.maps.MapTypeId.SATELLITE,
+         scaleControl: true,
+         scaleControlOptions: scaleOptions
+      };
+      
+      return new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+   }
+   
+   function makeLocationFromXml(region, item){
       var attributes = item.attributes;
       
       var id = attributes.getNamedItem("id").value;
@@ -100,12 +97,17 @@ function initialize() {
          mapCtrLatLng = latLng;
       }
       var zoom = parseInt(attributes.getNamedItem("zoom").value);
-      var loc = new Location(id, name, latLng, mapCtrLatLng, zoom);
+      return new Location(region, id, name, latLng, mapCtrLatLng, zoom);
+   };
       
+   function processLocation(loc, map) {
       add_li(loc);
       makeMarker(loc, map);
+   };
       
-      if(initLocId == id) {
+   // Side-Effect Warning: Updates global var initLoc!
+   function updateInitLoc(loc) {
+      if(initLocId == loc.id) {
          initLoc = loc;
       }
    };
@@ -116,14 +118,26 @@ function initialize() {
       return latLng;
    }
 
-   function Location(id, name, latLng, mapCtrLatLng, zoom) {
-      this.id = id;
+   function Location(region, id, name, latLng, mapCtrLatLng, zoom) {
       this.name = name;
       this.latlng = latLng;
       this.mapCtr = mapCtrLatLng;
       this.zoom = zoom;
+      
+      this.region = region;
+      
+      if(region == null) {
+         // The location is the region:
+         this.id = id;
+         this.title = "The "+this.name+" Sailing Locations";
+      } else {
+         this.id = region.id+"."+id;
+         this.title = "The "+region.name+" Sailing Locations: "+this.name;
+      }
+      
       this.path = basePath+"#"+this.id;
       this.action = makeLocationAction(this);
+      
    }
 
    function makeMarker(location, map) {
@@ -143,20 +157,24 @@ function initialize() {
       return function doSomething(event) {
          map.setCenter(mapCtr);
          map.setZoom(zoom);
-         document.title = htmlTitle+": "+location.name;
+         document.title = location.title;
          window.location.hash = location.id;
          setMapTitle(location.name);
          return false;
       }
+   }
+   
+   function modifyLocationLink(a, location) {
+      a.setAttribute('href', location.path);
+      a.onclick = location.action;
+      a.innerHTML = location.name;
    }
 
    function add_li(location) {
       var list = document.getElementById("locationList");
 
       var a = document.createElement('a');
-      a.setAttribute('href', location.path);
-      a.onclick = location.action;
-      a.innerHTML = location.name;
+      modifyLocationLink(a, location);
 
       var li = document.createElement("li");
       li.insertBefore(a, null);
